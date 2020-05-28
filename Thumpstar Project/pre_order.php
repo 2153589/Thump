d8660237f8ac57c71b50bb7ae8ff9a4045bcb08f
Michael Jericho Dariano <35998252+2153589@users.noreply.github.com>
2020-05-28 03:24:04 -0700

    Add files via upload

---
+++ Thumpstar Project/pre_order.php
@@ -0,0 +1,47 @@
+<?php
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        $lname = $_POST['last_name'];
+        $fname = $_POST['first_name'];
+        $email = $_POST['email'];
+        $address = $_POST['address'];
+        $mobile = $_POST['mobile'];
+        $model = $_POST['model'];
+
+        require __DIR__ . '/vendor/autoload.php';
+
+        $client = new Google_Client();
+        $client-> setApplicationName("Sample Add To Cart Function");
+        $client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
+        $client->setAccessType('offline');
+        $client->setAuthConfig(__DIR__ . '/credentials.json');
+    
+        $service = new Google_Service_Sheets($client);
+        $spreadSheetID = "12dfdyLC73FWC8vHr0gdSqBWxxgX7sthfY46iCpEkrNw";
+
+        $range = "orders";
+
+        $values = [
+            [$lname,$fname,$mobile,$email,$address,$model]
+        ];
+
+        $body = new Google_Service_Sheets_ValueRange([
+            "values" => $values
+        ]);
+
+        $params = [
+            'valueInputOption' => "RAW"
+        ];
+
+        $insert = [
+            "insertDataOption" => "INSERT_ROWS"
+        ];
+
+        $result = $service->spreadsheets_values->append($spreadSheetID,$range,$body,$params,$insert);
+
+        if($result){
+            echo "Order Successful";
+        }
+    }
+    
+
+?> 
